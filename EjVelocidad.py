3bb226881f18614b280846fcca9d6b3e84ab5353
FerGarciaG <46821657+FerGarciaG@users.noreply.github.com>
2019-01-22 11:04:26 -0600

    María Fernanda García Gastélum, Grupo 03

---
+++ EjVelocidad.py
@@ -0,0 +1,10 @@
+#Autor: Fernanda Garcia G
+#Calcula la velocidad promedio, dado el tiempo y distancia
+
+distancia = int(input("Teclea la distancia de tu viaje en kms enteros: "))
+tiempo = int(input("Teclea el tiempo de tu viaje en hrs enteras: "))
+
+velocidad= distancia/tiempo
+
+print("Velocidad promedio:", velocidad, "km/h")
+
